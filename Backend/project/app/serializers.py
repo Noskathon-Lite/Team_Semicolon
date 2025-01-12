1d1b7c15fa572b47b256289c20a19c42de9fa096
BishalParajulii <parajulibishal00@gmail.com>
2025-01-12 09:50:37 +0545

    login completed

---
+++ Backend/project/app/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from .models import User 
+from django.utils import timezone
+from datetime import date , timedelta
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'name', 'phone_number', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+             
+        }
+
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        validated_data['user_type'] = 'user'
+        user = User.objects.create(**validated_data)
+        if password:
+            user.set_password(password)  # Hash the password
+            user.save()
+        return user
+
+class UserLoginSerializer(serializers.Serializer):
+    phone_number = serializers.CharField()
+    password = serializers.CharField(write_only=True)
