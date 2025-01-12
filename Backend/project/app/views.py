db4b157060226bb2977c8b91c0584b4c492846d2
BishalParajulii <parajulibishal00@gmail.com>
2025-01-12 10:36:01 +0545

    police login completed

--- Backend/project/app/views.py
+++ Backend/project/app/views.py
@@ -21,7 +21,7 @@ from django.views import View
 import json
 from django.core.files.storage import FileSystemStorage
 from datetime import date , timedelta
-from .serializers import UserSerializer , UserLoginSerializer 
+from .serializers import UserSerializer , UserLoginSerializer , PoliceSerializer ,PoliceLoginSerializer
 from rest_framework.authentication import BasicAuthentication
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
@@ -73,5 +73,50 @@ class UserLoginView(APIView):
                     }, status=status.HTTP_200_OK)
 
             return Response({"message": "Invalid phone number or password"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RegisterPoliceView(APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = PoliceSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Account Created Successfully"},status=status.HTTP_201_CREATED,)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PoliceLoginView(APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = PoliceLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+            password = serializer.validated_data['password']
+            user = authenticate(email=email, password=password)
+
+            if user and isinstance(user, User): 
+                 if user.user_type == 'police':
+                    refresh = RefreshToken.for_user(user)
+
+                    user_details = {
+                        "name": user.name,
+                        "email": user.email,
+                        "phone_number": user.phone_number,
+                    }
+
+                    return Response({
+                        "message": "Login successful",
+                        "user_detail": user_details ,
+                        "access_token": str(refresh.access_token),
+                        "refresh_token": str(refresh),
+                    }, status=status.HTTP_200_OK)
+
+            return Response({"message": "Invalid email or password"}, status=status.HTTP_401_UNAUTHORIZED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- Backend/project/project/settings.py
+++ Backend/project/project/settings.py
@@ -29,9 +29,12 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "app",
     "rest_framework",
+    "corsheaders",
+    
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -141,3 +144,5 @@ SIMPLE_JWT = {
 AUTH_USER_MODEL = 'app.User'
 
 ALLOWED_HOSTS = ['*']
+
+CORS_ALLOWED_ORIGINS = []
