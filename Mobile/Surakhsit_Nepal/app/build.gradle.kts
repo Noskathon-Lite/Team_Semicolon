3993205ff927e296c3ff0a137978b63d7b140601
kiran <kiran.211720@ncit.edu.np>
2025-01-12 09:32:28 +0545

    added components for first setup

--- Mobile/Surakhsit_Nepal/app/build.gradle.kts
+++ Mobile/Surakhsit_Nepal/app/build.gradle.kts
@@ -56,4 +56,39 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //visibility ko lagi
+    implementation(libs.androidx.material.icons.extended)
+
+    //navigation ko lagi dependency
+    implementation(libs.androidx.navigation.compose)
+
+    // grid layout dependecy
+    implementation(libs.androidx.foundation)
+
+    //viewModel ko lagi
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+
+    //retrofit api ko lagi
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    //image dekhauna laii coil dependency
+    implementation(libs.coil.compose)
+
+    //okhttp dependency
+    implementation(libs.okhttp)
+    implementation(libs.logging.interceptor)
+    implementation(platform(libs.okhttp.bom))
+
+    //location ko lagi
+    implementation(libs.play.services.location)
+    implementation(libs.play.services.maps)
+
+    //permission ko lagi
+    implementation(libs.accompanist.permissions)
+
+    // phone number ko lagi
+    implementation(libs.play.services.auth)
+
 }
---
+++ Mobile/Surakhsit_Nepal/app/src/main/java/com/example/surakhsit_nepal/OnBoardingScreens/OnBoardingModel.kt
@@ -0,0 +1,4 @@
+package com.example.surakhsit_nepal.OnBoardingScreens
+
+class OnBoardingModel {
+}
