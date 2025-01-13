44c88d60fbfe044817a6adeaf35bd59bb5561b3b
kiran <kiran.211720@ncit.edu.np>
2025-01-13 11:33:42 +0545

    wanted list

--- Mobile/Surakhsit_Nepal/app/src/main/java/com/example/surakhsit_nepal/CriminalDatabase/CriminalViewModel.kt
+++ Mobile/Surakhsit_Nepal/app/src/main/java/com/example/surakhsit_nepal/CriminalDatabase/CriminalViewModel.kt
@@ -1,3 +1,2 @@
 package com.example.surakhsit_nepal.CriminalDatabase
 
-class criminals : ArrayList<criminalsItem>()
--- Mobile/Surakhsit_Nepal/app/src/main/java/com/example/surakhsit_nepal/CriminalDatabase/criminalsItem.kt
+++ Mobile/Surakhsit_Nepal/app/src/main/java/com/example/surakhsit_nepal/CriminalDatabase/criminalsItem.kt
@@ -1,10 +1,9 @@
 package com.example.surakhsit_nepal.CriminalDatabase
 
 data class criminalsItem(
-    val age : Int,
-    val case : String,
-    val gender : String,
-    val name : String,
-    val image : String
-
+    val name: String,
+    val age: Int,
+    val gender: String,
+    val case: String,
+    val image: String
 )
